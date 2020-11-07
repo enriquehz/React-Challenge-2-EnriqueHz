1464cd37f9b4ed69ce0968e2de4bfe5b5af3832d
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-11-06 20:14:14 -0600

    Working Hover and video lonk

--- src/components/PokeCard.js
+++ src/components/PokeCard.js
@@ -1,31 +1,18 @@
 import React from "react";
-//import { mockPokemonsData } from "../mock/pokeData";
-
-//console.log(mockPokemonsData);
 
 function PokeCard(props) {
-  //let pokemonName = props.name;
-  //let spriteDefault = props.sprites.front_default;
-  //let spriteShiny = props.sprites.front_shiny;
-  //let pokemonVideo = props.video;
-
-  /*
-  let pokemonName = mockPokemonsData[0].name;
-  let spriteDefault = mockPokemonsData[0].sprites.front_default;
-  let spriteShiny = mockPokemonsData[0].sprites.front_shiny;
-  let pokemonVideo = mockPokemonsData[0].videoLink;
-  */
-
   return (
     <div>
       <h1> {props.pokemonName} </h1>
       <p>
-        <img src={props.spriteDefault} alt="Default" />
+        <img
+          src={props.spriteDefault}
+          onMouseOver={(e) => (e.currentTarget.src = props.spriteShiny)}
+          onMouseOut={(e) => (e.currentTarget.src = props.spriteDefault)}
+          onClick={() => window.open(props.pokemonVideo, "_blank")}
+          alt="Default"
+        />
       </p>
-      <p>
-        <img src={props.spriteShiny} alt="Shiny" />
-      </p>
-      <a href={props.pokemonVideo}>{props.pokemonName} video</a>
     </div>
   );
 }
