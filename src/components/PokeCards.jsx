2d2cd468e07c18bb0327ea558da13b7765788a5c
Enrique Hernandez <enrique.hz.t@gmail.com>
2020-11-06 20:45:55 -0600

    Almost there

--- src/components/PokeCards.jsx
+++ src/components/PokeCards.jsx
@@ -8,6 +8,38 @@ let pokeName = items[0].name;
 console.log(items[0]);
 console.log(pokeName);
 
+//const listItems = items.map((item) => item);
+
+let itemList = [];
+items.forEach((item, index) => {
+  itemList.push(
+    <PokeCard
+      pokemonName={item.name}
+      spriteDefault={item.sprites.front_default}
+      spriteShiny={item.sprites.front_shiny}
+      pokemonVideo={item.videoLink}
+    />
+  );
+});
+
+console.log(itemList);
+function PokeCards() {
+  return <div>{itemList}</div>;
+}
+
+export default PokeCards;
+
+/*
+import React from "react";
+import PokeCard from "./PokeCard";
+import { mockPokemonsData } from "../mock/pokeData";
+
+let items = mockPokemonsData;
+let pokeName = items[0].name;
+
+console.log(items[0]);
+console.log(pokeName);
+
 function PokeCards() {
   return (
     <div>
@@ -22,3 +54,4 @@ function PokeCards() {
 }
 
 export default PokeCards;
+*/
